21d88976605074f7253be367bba397560e35b406
Meghan <meghan.frost89@gmail.com>
2018-09-10 14:29:29 -0500

    Link into Napster API and test Spotify API
    Get Napster API working, test node.js and Spotify api

--- js/app.js
+++ js/app.js
@@ -9,8 +9,9 @@ window.onload = function() {
 function connectToDarksky () {	
 
 var darkskyRequest = new XMLHttpRequest();
+	weatherLocation = '41.8781,-87.6298';
 	
-darkskyRequest.open('GET', 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/6442aa0485c1e820626c77adfa9364f6/41.8781,-87.6298');
+darkskyRequest.open('GET', 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/6442aa0485c1e820626c77adfa9364f6/' + weatherLocation );
 darkskyRequest.send();
 
 function errorHandler() {
@@ -29,37 +30,36 @@ darkskyRequest.onerror = errorHandler;
 darkskyRequest.onload = weatherContent;
 
 }
+
 connectToDarksky();
 
 
 
-function connectToSpotify () {
+function connectToNapster () {
 
-var client_id = '4b2c0e196691470592e7d96f7d4de59d'; // Your client id
-var client_secret = '06c41e9a2db04dacadf10e6e7e6f7577'; // Your secret
-var redirect_uri = ''; // Your redirect uri
-
-var spotifyRequest = new XMLHttpRequest();
+	var napsterRequest = new XMLHttpRequest();
 	
-spotifyRequest.open('GET', 'https://api.spotify.com/v1/tracks/4b2c0e196691470592e7d96f7d4de59d/BQDgKXK2anzIZ2YAC69vcVIDS5q1-jb_3MdamIb4hZMAm88Ozq-Q-VlTuQcZF6AD1RB96yomRR1iDjCDfKBP7kWeNtoBoDt9jI9r1AsTvWA-sHckWbKvLfYOtblHDM2ZEKs4B-HO-i_QK_s');
-spotifyRequest.send();
+napsterRequest.open('GET', 'https://api.napster.com//v2.2/genres?apikey=Yjk4MjE0NTAtZmE5Yi00NzY5LTljNWUtNzA2ZDI5NzQyYjNl');
+napsterRequest.send();
 
 function errorHandler() {
   console.log('something went wrong');
 }
 
-function weatherContent () {
-	var spotifyData = JSON.parse(spotifyRequest.responseText);
-	console.log(spotifyData);
-}
-
-
-spotifyRequest.onerror = errorHandler;
-spotifyRequest.onload = weatherContent;
+function napsterContent () {
+	var musicData = JSON.parse(napsterRequest.responseText);
+	console.log (musicData);
 
 }
 
-connectToSpotify();
+
+napsterRequest.onerror = errorHandler;
+napsterRequest.onload = napsterContent;
+
+}
+
+connectToNapster();
+
 
 
 
